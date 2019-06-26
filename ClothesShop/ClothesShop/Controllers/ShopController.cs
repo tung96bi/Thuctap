67bc3cdb703c1d4504a37f2900ca182210e18fb1
Mai Xuan Tung <tungmx@psnet.asia>
2019-06-26 14:41:58 +0700

    a

---
+++ ClothesShop/ClothesShop/Controllers/ShopController.cs
@@ -0,0 +1,33 @@
+﻿using ClothesShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothesShop.Controllers
+{
+    public class ShopController : Controller
+    {
+        private TestEntities db = new TestEntities();
+        // GET: Shop
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowMenu()
+        {
+            var model = (from u in db.Catalogs where u.fcatalogID == null select u ).ToList();
+            var fmodel = (from u in db.Catalogs where u.fcatalogID == 2 select u).ToList();
+            ViewBag.Fmodel = fmodel;
+            return PartialView("ShowMenu", model);
+        }
+
+        //public ActionResult _ShopLayout()
+        //{
+        //    var model = (from u in db.Catalogs where u.fcatalogID == null select u).ToList();
+        //    return View(model);
+        //}
+    }
+}
