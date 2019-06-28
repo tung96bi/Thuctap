db3b06a1ad9d3ab18b40a986ef59f6663ddb308f
Mai Xuan Tung <tungmx@psnet.asia>
2019-06-28 13:20:07 +0700

    Sua project 28/06/2019

--- ClothesShop/ClothesShop/Controllers/ShopController.cs
+++ ClothesShop/ClothesShop/Controllers/ShopController.cs
@@ -29,5 +29,6 @@ namespace ClothesShop.Controllers
            var model = (from u in db.Catalogs where u.fcatalogID == null select u).ToList();
             return PartialView("Footer", model);
         }
+        //ABCCCC
     }
 }
